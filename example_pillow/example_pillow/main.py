97c36e1330fb9829a3f5ff5adbf1350032853688
Theo Bob Massard <tbobm@protonmail.com>
2025-01-23 13:51:40 +0100

    feat(stats): include watermark only
    Signed-off-by: Theo Bob Massard <tbobm@protonmail.com>

--- example_pillow/example_pillow/main.py
+++ example_pillow/example_pillow/main.py
@@ -1,4 +1,9 @@
+import datetime
+import functools
+
+s = datetime.datetime.utcnow()
 from PIL import Image
+print("Pillow module loaded in", datetime.datetime.utcnow() - s)
 
 SOURCE_IMAGE = "../assets/source.jpg"
 OVERLAY_IMAGE = "../assets/overlay.png"
@@ -14,5 +19,18 @@ def main_pil():
     source.save(RESULT_IMAGE)
 
 
+def watermark_only():
+    source = Image.open(SOURCE_IMAGE)
+    overlay = Image.open(OVERLAY_IMAGE)
+
+    position_x, position_y = (source.width - overlay.width) // 2, (source.height - overlay.height) // 2
+
+    import timeit
+    to_test = functools.partial(source.paste, overlay, (position_x, position_y), overlay)
+    res = timeit.timeit(to_test, number=100000)
+    print(res)
+
+
 if __name__ == "__main__":
     main_pil()
+    watermark_only()
--- example_pyvips/example_pyvips/main.py
+++ example_pyvips/example_pyvips/main.py
@@ -1,4 +1,9 @@
+import datetime
+import functools
+
+s = datetime.datetime.utcnow()
 import pyvips
+print("PyVips module loaded in", datetime.datetime.utcnow() - s)
 
 SOURCE_IMAGE = "../assets/source.jpg"
 OVERLAY_IMAGE = "../assets/overlay.png"
@@ -11,9 +16,21 @@ def main_vips():
 
     position_x, position_y = (source.width - overlay.width) // 2, (source.height - overlay.height) // 2
 
-    watermarked = source.composite2(overlay, "over", x=position_x, y=position_y)
+    watermarked = source.composite2( overlay, "over", x=position_x, y=position_y)
     watermarked.write_to_file(RESULT_IMAGE)
 
 
+def watermark_only():
+    source = pyvips.Image.new_from_file(SOURCE_IMAGE)
+    overlay = pyvips.Image.new_from_file(OVERLAY_IMAGE)
+
+    position_x, position_y = (source.width - overlay.width) // 2, (source.height - overlay.height) // 2
+
+    import timeit
+    to_test = functools.partial(source.composite2, overlay, "over", x=position_x, y=position_y)
+    res = timeit.timeit(to_test, number=100000)
+    print(res)
+
 if __name__ == "__main__":
     main_vips()
+    watermark_only()
